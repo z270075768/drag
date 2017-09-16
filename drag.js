b382657dc207950b2dc1f536bd4647d9059d5ada
z270075768 <270075768@qq.com>
2017-09-16 20:18:09 +0800

    change drag.js from master

--- drag.js
+++ drag.js
@@ -5,5 +5,9 @@ function drag(id){
 	obj.onmousedown = function(){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
-	}
+		document.onmousemove = function(){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		};
+	};
 }
